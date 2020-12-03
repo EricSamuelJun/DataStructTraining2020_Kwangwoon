3607ba3c6f7d8972c2b62d005375be5d0eef6175
Jang-HW <66403495+Jang-HW@users.noreply.github.com>
2020-12-04 02:25:38 +0900

    Update main.cpp
    수정한거 다 삭제

--- .vs/DataStructTraining2020_Kwangwoon/v16/main.cpp
+++ .vs/DataStructTraining2020_Kwangwoon/v16/main.cpp
@@ -16,29 +16,5 @@ int main(int argc, char *argv[]) {
     // 3. callScreen
     // 4.
 
-    int selecMenu;
-
-    while (1) {
-        printf("1. alarm\n: ");
-        scanf("%d", &selecMenu);
-        switch (selecMenu) {
-        case 1:
-            printf("1. how to		2. set alarm\n: ");
-            scanf("%d", &selecMenu);
-            switch (selecMenu) {
-            case 1:
-                howToAlarm();
-                break;
-            case 2:
-                alarm_client();
-                break;
-            }
-            break;
-
-        default:
-            break;
-        }
-        printf("\n");
-    }
     return 0;
 }
